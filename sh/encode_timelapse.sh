a95c602bf0966057310315f9bcd0ef4ea74f037e
Jackson Westeen <jcw21@email.arizona.edu>
2017-02-06 21:22:02 -0500

    shebang

--- sh/encode_timelapse.sh
+++ sh/encode_timelapse.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 /usr/local/bin/ffmpeg -framerate 20 -pattern_type glob -i '/var/www/html/frames/*.jpg' -vcodec h264_omx -b:v 1500k -y /var/www/html/tmp.mp4
 mv /var/www/html/tmp.mp4 /var/www/html/plantz.mp4
