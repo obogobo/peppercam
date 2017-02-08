cf47d875a07c2897e79a177372e487d365787f48
Jackson Westeen <jcw21@email.arizona.edu>
2017-02-07 23:58:27 -0500

    boot scripts

--- sh/capture_still.sh
+++ sh/capture_still.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/local/bin/ffmpeg -y -i "http://:8080/video/stream.mjpg" -r 1 -vframes 1 "/var/www/html/frames/$(date +%s).jpg" > /home/pi/capture.log 2>&1
+/usr/local/bin/ffmpeg -y -i "http://:8080/video/stream.mjpg" -r 1 -vframes 1 "/var/www/html/frames/$(date +%s).jpg" |& tee -a /home/pi/snapshot.log
